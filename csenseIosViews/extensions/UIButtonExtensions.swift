fbdf087232a647647a2765ffe2ab6dc1921c249a
Kasper Tvede <kasper@webmasteren.eu>
2017-11-20 23:18:53 +0100

    more extensions.

---
+++ csenseIosViews/extensions/UIButtonExtensions.swift
@@ -0,0 +1,18 @@
+//
+// Created by Kasper T on 20/11/2017.
+// Copyright (c) 2017 commonsense. All rights reserved.
+//
+
+import Foundation
+@IBDesignable
+public extension UIButton {
+    @IBInspectable
+    var title: String {
+        get {
+            return currentTitle ?? ""
+        }
+        set(newTitle) {
+            setTitle(newTitle, for: .normal)
+        }
+    }
+}
--- csenseIosViews/extensions/UIViewExtensions.swift
+++ csenseIosViews/extensions/UIViewExtensions.swift
@@ -2,26 +2,56 @@
 // Created by Kasper T on 06/11/2017.
 // Copyright (c) 2017 commonsense. All rights reserved.
 //
+
 import csenseSwift
 import csenseIosBase
 import Foundation
+
 public extension UIView {
-    
-    public func animateAlpha(alphaValue : CGFloat){
-        if(alphaValue > 1 || alphaValue < 0 ){
+
+    public func animateAlpha(alphaValue: CGFloat) {
+        if alphaValue > 1 || alphaValue < 0 {
             logWarning(message: "Skipping animation, alpha value is out of range (should be [0;1] but is \(alphaValue)")
             return
         }
-        animateView{ [weak self ] in
+        animateView {
+            [weak self ] in
             self?.alpha = alphaValue
             self?.setNeedsDisplay()
         }
     }
-    
+
     public func animateView(duration: CGFloat = UINavigationControllerHideShowBarDuration,
                             action: @escaping EmptyFunction) {
-            UIView.animate(withDuration: TimeInterval(duration), animations: action)
+        UIView.animate(withDuration: TimeInterval(duration), animations: action)
     }
-    
-    
+
+    @IBInspectable
+    public var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set(value) {
+            self.layer.cornerRadius = value
+        }
+    }
+
+    public func makeRound() {
+        self.layer.cornerRadius = frame.height / 2.0
+    }
+
+    @IBInspectable
+    public var isRound: Bool {
+        get {
+            return (cornerRadius.isEqual(to: frame.height / 2.0, withStrife: 1))
+        }
+        set(value) {
+            if value {
+                makeRound()
+            } else {
+                cornerRadius = 0
+            }
+        }
+    }
+
 }
